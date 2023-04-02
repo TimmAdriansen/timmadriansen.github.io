5db410539eafaef51ffbd48c54b326d988aacef4
Andreas Timm Adriansen <andreaserchang@gmail.com>
2023-04-02 19:29:44 +0200

    centered text and js script for handling hover

--- onhover.js
+++ onhover.js
@@ -1,7 +1,23 @@
 function onHoverImage(x) {
-    x.style.opacity = "0.7";
+    let image = x.querySelector(".image");
+    let div = x.querySelector(".centered");
+    image.style.opacity = "0.7";
+    image.style.filter = "grayscale(100%)";
+    showTextHover(div);
 }
 
 function onHoverImageLeave(x) {
-    x.style.opacity = "1";
+    let image = x.querySelector(".image");
+    let div = x.querySelector(".centered");
+    image.style.opacity = "1";
+    image.style.filter = "grayscale(0%)";
+    hideTextHover(div);
+}
+
+function showTextHover(div){
+    div.style.opacity = "1";
+}
+
+function hideTextHover(div){
+    div.style.opacity = "0";
 }
